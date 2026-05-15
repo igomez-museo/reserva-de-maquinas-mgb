a1d1fe349fb1f0c3797ff67208e763f787f1a33f
Ignacio Gomez <IGOMEZ@guggenheim-bilbao.eus>
2026-05-15 11:10:03 +0200

    disabled mock mode

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const spConfig = {
 // ==========================================
 // MODO MOCK (Simulación sin conexión a API)
 // Cambia a 'false' cuando configures los IDs reales.
-const MOCK_MODE = true;
+const MOCK_MODE = false;
 // ==========================================
 
 let myMSALObj;
